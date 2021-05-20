d544245b3673212ebf359e75ef0bc0b226accc32
ji3eon <77104208+ji3eon@users.noreply.github.com>
2021-05-20 11:03:54 +0900

    update Cpp_class file

--- Cpp_class/Linked_List.cpp
+++ Cpp_class/Linked_List.cpp
@@ -1,66 +1,85 @@
 #include <iostream>
 using namespace std;
 
-typedef struct Node {
-	int value;
-	struct Node* next;
-};
-
-class primary {
+class Node {
 private:
+	int value;
+	Node* next = NULL;
 	Node* head = NULL;
-public:
+	Node* tail = NULL;
+
+public: 
 	void print(Node *head) { //전체 원소 출력
 		for (Node* p = head; p != NULL; p = p->next)
-			cout << p->value;
+			cout << p->value<<" -> ";
 		cout << "\n";
 	}
 	void push_back(Node *tail, int value) { //맨 뒤에 추가
-		if (next == NULL) { //next가 NULL이면 노드의 마지막
-			head = (Node*)malloc(sizeof(Node)); //새로운 노드 생성
-			Node* next = Node;
+		Node* p = (Node*)malloc(sizeof(Node)); //새로운 노드 생성
+		p->value = value; //값을 넣고
+		if (head == NULL) { //head가 NULL이면 생성된 노드 없음 -> 노드 생성
+			Node* head = p;
+		}
+		else { //head가 NULL이 아니면(=노드가 하나라도 생성된 경우)
+			Node* next = p; //next가 가장 마지막에 생성된 노드를 가리키도록 함
+		}
+		Node* tail = p; //tail은 항상 연결리스트 마지막 노드를 가리키도록 설정
+	}
+	void pop(Node *tail,int value) { //맨 뒤에 삭제
+		Node* delp;
+		if (tail != NULL) { //노드가 하나라도 생성된 경우
+			delp = tail;
+			free(delp);
+			tail=
 		}
 		else {
-
+			cout << "더이상 삭제할 노드는 없습니다.";
 		}
-		Node* p = (Node*)malloc(sizeof(Node));
-		p->value = value;
-		p->next = p;
 	}
-	void pop() { //맨 뒤에 삭제
+	
+};
+
+class Single_Linked_List : public Node {
+
+};
+
+class Single_Circular_Linked_List : public Node {
+
+};
+
+class Double_Linked_List : public Node{
+	virtual void push_front(Node* front,int value) { //맨 앞에 추가 
+		Node* p = (Node*)malloc(sizeof(Node)); //새로운 노드 생성
+		p->value = value; //FIXME : value가 private으로 선언되어있어서 수정 필요
 
 	}
-	void push_front() { //맨 앞에 추가 (예외처리 가능하도록 - 이중 연결시에만)
-
-	}
-	void pop_front() { //맨 앞에 삭제 (예외처리 가능하도록 - 이중 연결시에만)
+	virtual void pop_front() { //맨 앞에 삭제 
 
 	}
 };
 
-class Single_Linked_List : public primary {
+class Double_Circular_Linked_List : public Node{
+	virtual void push_front() { //맨 앞에 추가
 
-};
+	}
+	virtual void pop_front() { //맨 앞에 삭제
 
-class Single_Circular_Linked_List : public primary {
-
-};
-
-class Double_Linked_List : public primary {
-
-};
-
-class Double_Circular_Linked_List : public primary {
-
+	}
 };
 
 int main() {
 	int n;
+	cout<<"단일_선형은 1번"
 	cin >> n;
 
+
+	
 	switch (n) {
+		int num;
 		if (n == 1) {
-			Single_Linked_List SLL
+			cout << "전체원소 출력은 1번" << "\n맨뒤에 추가는 2번" << "\n맨뒤 삭제는 3번" << "을 눌러주세요";
+			num=
+			Single_Linked_List SLL;
 		}
 		else if (n == 2) {
 
